7a09daaf4191877633dd138f9d6011c4b8d34420
Šimon Jurdič <Jurdic.Simon@stud.vassboskovice.cz>
2021-10-26 09:23:39 +0200

    Přidejte soubory projektu.

---
+++ vek/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace vek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double age;
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.SetCursorPosition(15, 10);
+            Console.Write("╔═════════════════╗");
+
+            Console.SetCursorPosition(15, 11);
+            Console.WriteLine("║napište svůj věk ║");
+
+            Console.SetCursorPosition(15, 12);
+            Console.Write("╚═════════════════╝");
+
+            Console.SetCursorPosition(15, 15);
+            Console.ReadLine();
+
+
+           
+
+        }
+    }
+}
